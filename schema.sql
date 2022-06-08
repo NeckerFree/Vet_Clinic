573dc2145c009e181a644165b81d193f163eadee
Elio Cortes <elio.cortes3000@gmail.com>
2022-06-08 13:20:51 -0500

    query multiple tables

--- schema.sql
+++ schema.sql
@@ -14,3 +14,21 @@ weight_kg decimal
 -- Add a column species of type string
 ALTER TABLE animals 
 ADD COLUMN species VARCHAR NULL;
+
+
+-- query multiple tables
+create table owners (
+    id SERIAL PRIMARY KEY,
+    full_name varchar(100) NOT NULL,
+    age INT NOT NULL
+);
+
+create table species (
+    id SERIAL PRIMARY KEY,
+    name varchar(100) NOT NULL
+);
+
+--Modify animals table
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INT CONSTRAINT FK_animals_species REFERENCES species (id);
+ALTER TABLE animals ADD COLUMN owner_id INT CONSTRAINT FK_animals_owners REFERENCES owners (id);
